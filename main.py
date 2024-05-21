4ebf2ed64a3f1dcad00e085e5779e1676482c2db
André Luiz <andreluizgomes43@hotmail.com>
2024-05-21 08:58:14 -0300

    primeira versao

---
+++ main.py
@@ -0,0 +1,33 @@
+import tkinter
+from tkinter import messagebox as mb
+from tkinter import ttk
+import sqlite3
+
+def chamarBotao():
+    # Função de exemplo para o botão 'Salvar'
+    print("Botão Salvar pressionado")
+
+def main():
+    root = tkinter.Tk()
+    root.title("Trabalho RAD")
+    root.resizable(False, False)
+    
+    label = tkinter.Label(root, text="Nome")
+    label.pack()
+
+    textoEntrada = tkinter.StringVar()
+    e1 = tkinter.Entry(root)
+    e1.bind('<Key>', lambda x:textoEntrada.set(e1.get()+x.char))
+    e1.pack()
+    
+    test2 = tkinter.Button(root, text="Salvar")
+    test2['command'] = chamarBotao
+
+    test2.pack()
+
+    root.iconify() 
+    root.update()
+    root.deiconify() 
+    root.mainloop()
+
+main()
